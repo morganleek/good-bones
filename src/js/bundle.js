8f3bc70ac8ac7d56c4ae066092e78ed60f025217
morganleek <morganleek@gmail.com>
2021-05-03 05:12:17 +0000

    favicons

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,5 +1,25 @@
 // import $ from 'jquery';
 
 // $(function() {
-// 	// jQuery DOM Ready
+	// // Favicons
+	// const favicons = '<link rel="apple-touch-icon" sizes="180x180" href="%path%/apple-touch-icon.png">' +
+	// '<link rel="icon" type="image/png" sizes="192x192"  href="%path%/android-chrome-192x192.png">' +
+	// '<link rel="icon" type="image/png" sizes="512x512"  href="%path%/android-chrome-512x512.png">' +
+	// '<link rel="icon" type="image/png" sizes="32x32" href="%path%/favicon-32x32.png">' +
+	// '<link rel="icon" type="image/png" sizes="16x16" href="%path%/favicon-16x16.png">';
+
+	// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	// 	// Dark Mode
+	// 	document.head.insertAdjacentHTML(
+	// 		'beforeend',
+	// 		favicons.replace(/%path%/g, site.template_directory + '/dist/images/favicon/favicon-dark')
+	// 	);
+	// }
+	// else {
+	// 	// Light Mode
+	// 	document.head.insertAdjacentHTML(
+	// 		'beforeend',
+	// 		favicons.replace(/%path%/g, site.template_directory + '/dist/images/favicon/favicon-light')
+	// 	);
+	// }
 // });
